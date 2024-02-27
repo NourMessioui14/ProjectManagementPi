1faccfe3b5d31f36ca7fc3bb5915242dfd894ee5
nour <nour.messioui@esprit.tn>
2024-02-27 22:17:46 +0100

    Revert "user"
    This reverts commit b5c0e87873bd0c3496936f85cfa64225055b3734.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3001', // Mettez ici l'URL de votre application frontend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  
-  app.useGlobalPipes(new ValidationPipe());
-
   await app.listen(3000);
 }
 bootstrap();
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './Backoffice/pages/Dashboard';
-import Login from './FrontOffice/pages/Login';
-import Register from './FrontOffice/pages/Register';
-import Navbar from './Backoffice/components/Navbar';
-import Sidebar from './Backoffice/components/Sidebar';
-import Footer from './Backoffice/components/Footer';
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route
-          path="/backoffice/*"
-          element={
-            <div className='wrapper'>
-              <Sidebar />
-              <div className="main">
-                <Navbar />
-                <main className="content">
-                  <div className="container-fluid p-0">
-                    <Routes>
-                      <Route index element={<Dashboard />} />
-                      {/* Ajoutez d'autres routes spécifiques au back office ici */}
-                    </Routes>
-                  </div>
-                </main>
-                <Footer />
-              </div>
-            </div>
-          }
-        />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        {/* Ajoutez d'autres routes spécifiques au front office ici */}
+        {/* eli yheb yaamel route yhotha hne */}
       </Routes>
-    </Router>
+      
+      <div className='wrapper'>
+        <Sidebar />
+        <div className="main">
+          <Navbar />
+          <main className="content">
+            <div className="container-fluid p-0">
+              <Dashboard />
+            </div>
+          </main>
+          <Footer />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
