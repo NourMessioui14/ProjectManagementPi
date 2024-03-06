9f7d958f8fca0cfbb76c6b45f7fd788e0c6f79fe
malikrmedi <malik.rmedi@esprit.tn>
2024-03-06 11:00:48 +0100

    crudbien

--- ProjectManagementPi/backend/src/main.ts
+++ ProjectManagementPi/backend/src/main.ts
@@ -4,7 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
+
+
   app.setGlobalPrefix('/api');
+
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
 
 
   const port = process.env.PORT;
