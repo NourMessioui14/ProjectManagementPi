b90c796bf1a161577c4bee35883aa24b3556f645
NourMessioui14 <124040329+NourMessioui14@users.noreply.github.com>
2024-05-09 22:20:54 +0200

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,30 +1,25 @@
-
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 import * as session from 'express-session';
 import * as passport from 'passport';
 import * as path from 'path';
 import * as cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors({
-    origin: 'https://rococo-otter-2b11ca.netlify.app/',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  
+  // Configuration CORS
+  const corsOptions: CorsOptions = {
+    origin: 'https://rococo-otter-2b11ca.netlify.app',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     credentials: true,
-    exposedHeaders: 'Set-Cookie',
-
-    allowedHeaders: [
-      'Access-Control-Allow-Origin',
-      'Content-Type',
-      'Authorization',
-    ],
-
-  });
-  app.useGlobalPipes(new ValidationPipe());
+  };
+  app.enableCors(corsOptions);
+  
+  // Configuration des sessions
   app.use(
     session({
       name: 'NestJs_Session_Id',
@@ -34,13 +29,22 @@ async function bootstrap() {
       cookie: { maxAge: 600000 },
     }),
   );
+
+  // Utilisation de cookie-parser
   app.use(cookieParser());
 
+  // Initialisation de Passport.js
   app.use(passport.initialize());
   app.use(passport.session());
-  app.useStaticAssets(path.join(__dirname , './uploads'));
 
+  // Configuration du serveur statique pour les téléversements
+  app.useStaticAssets(path.join(__dirname, './uploads'));
 
+  // Utilisation des tuyaux de validation globaux
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Écoute du port 5001
   await app.listen(5001);
 }
+
 bootstrap();
